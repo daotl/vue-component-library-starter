f5032d618d66391e535f7bcc66249d61c8bb61e0
lifang <2393477099@qq.com>
2023-12-21 14:29:06 +0800

    feat: add @nx/storybook

--- src/types/typed-router.d.ts
+++ src/types/typed-router.d.ts
@@ -41,9 +41,7 @@ declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/[...all]': RouteRecordInfo<'/[...all]', '/:all(.*)', { all: ParamValue<true> }, { all: ParamValue<false> }>,
-    '/about': RouteRecordInfo<'/about', '/about', Record<never, never>, Record<never, never>>,
     '/hi/[name]': RouteRecordInfo<'/hi/[name]', '/hi/:name', { name: ParamValue<true> }, { name: ParamValue<false> }>,
-    '/README': RouteRecordInfo<'/README', '/README', Record<never, never>, Record<never, never>>,
   }
 }
 
@@ -136,6 +134,14 @@ declare module 'vue-router' {
   export interface TypesConfig {
     beforeRouteUpdate: NavigationGuard<RouteNamedMap>
     beforeRouteLeave: NavigationGuard<RouteNamedMap>
+
+    $route: RouteLocationNormalizedLoadedTypedList<RouteNamedMap>[keyof RouteNamedMap]
+    $router: _RouterTyped<RouteNamedMap>
+
+    RouterLink: RouterLinkTyped<RouteNamedMap>
+  }
+}
+NamedMap>
 
     $route: RouteLocationNormalizedLoadedTypedList<RouteNamedMap>[keyof RouteNamedMap]
     $router: _RouterTyped<RouteNamedMap>
